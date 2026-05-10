46560ab889f6d3bdb565989147a77d0947e99f25
TylerPete <tpeterson4@byupathway.edu>
2026-05-10 00:42:25 -0500

    meow once more

--- routes/professional.js
+++ routes/professional.js
@@ -3,3 +3,6 @@ const router = express.Router();
 const professionalController = require("../controllers/professionalController");
 
 router.get("/", professionalController.getData);
+
+
+module.exports = router;

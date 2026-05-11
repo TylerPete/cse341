18a363fa0efbc5e984baa485487abaa99ec521e1
TylerPete <tpeterson4@byupathway.edu>
2026-05-11 18:24:30 -0500

    trying to keep figuring out more about what the heck is going on with mongodb stuff

--- controllers/professionalController.js
+++ controllers/professionalController.js
@@ -1,31 +1,7 @@
 const mongodb = require("../database/connect");
 
 const getData = async (req, res, next) => {
-    //send hardcoded object with professional attributes needed
-
-    // const dataObj = {}
-
-    // dataObj.professionalName = "professionalName"
-    // dataObj.base64Image = "base64Image"
-    // dataObj.primaryDescription = "primaryDescription"
-    // dataObj.workDescription1 = "workDescription1"
-    // dataObj.workDescription2 = "workDescription2"
-    // dataObj.linkTitleText = "linkTitleText"
-    // dataObj.nameLink = {
-    //     firstName: "firstName",
-    //     url: "#"
-    // };
-    // dataObj.linkedInLink = {
-    //     text: "text",
-    //     link: "link"
-    // };
-    // dataObj.githubLink = {
-    //     text: "text",
-    //     link: "link"
-    // }
-
-    dataObj = mongodb.getData();
-    res.json(dataObj);
+    // meow work on stuff here
 };
 
 
--- database/connect.js
+++ database/connect.js
@@ -1,17 +1,30 @@
 const {MongoClient} = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config;
 
-async function getData(req, res, next) {
-    // connection string
-    const uri = "mongodb://tylerpete16:Plinkming-Bloop%2A247@ac-xb9202g-shard-00-00.behjctk.mongodb.net:27017,ac-xb9202g-shard-00-01.behjctk.mongodb.net:27017,ac-xb9202g-shard-00-02.behjctk.mongodb.net:27017/?ssl=true&replicaSet=atlas-tlg17r-shard-0&authSource=admin&appName=cse341"
+let _db;
 
-    const client = new MongoClient(uri);
+const initDb = (callback) => {
+    if (_db) {
+        console.log('Db is already initialized!');
+        return callback(null, _db);
+    }
+    MongoClient.connect(process.env.MONGODB_URI)
+        .then((client) => {
+            _db = client;
+            callback(null, _db);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+}
 
-    const data = client.db("professional").collection("person").find();
-
-    console.log("Data: ", data)
-
-    return data;
+const getDb = () => {
+    if (!_db) {
+        throw Error('Db not initialized');
+    }
+    return _db;
 }
 
 
-module.exports = {getData}
+module.exports = { initDb, getDb};
